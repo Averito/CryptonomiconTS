6d161d85980ea4e4b9a924a9b35656f025f90c6b
Averito <avenantmar@gmail.com>
2022-10-27 00:16:27 +0300

    prettier fixes

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -4,8 +4,7 @@ export const API = {
 	async getCrypto(url: string) {
 		const crypto = await axios.get(`${url}/blockchain/list`, {
 			headers: {
-				authorization:
-					'Apikey b180c293c4268e85395f7cde4c396178b7cc4755fd3227614755fa676106a8d2'
+				authorization: 'Apikey b180c293c4268e85395f7cde4c396178b7cc4755fd3227614755fa676106a8d2'
 			}
 		});
 		return crypto.data.Data;
@@ -13,8 +12,7 @@ export const API = {
 	async getCostBySymbol(url: string, symbol: string) {
 		const costCoin = await axios.get(`${url}/price?fsym=${symbol}&tsyms=USD`, {
 			headers: {
-				authorization:
-					'Apikey b180c293c4268e85395f7cde4c396178b7cc4755fd3227614755fa676106a8d2'
+				authorization: 'Apikey b180c293c4268e85395f7cde4c396178b7cc4755fd3227614755fa676106a8d2'
 			}
 		});
 		return costCoin.data;
